3c91c171fe9057ed1b4f0da8756908df38d035d1
Lucas Renan <lucasrenan365@outlook.com>
2023-05-26 23:10:01 -0300

    icone de close atualizado

--- src/components/Brands/index.tsx
+++ src/components/Brands/index.tsx
@@ -1,17 +1,27 @@
-import './styles.scss'
-import brands from '/brands.svg'
+import "./styles.scss";
+import brands from "/brands.svg";
 
 export const Brands = () => {
-    return(
-        <div className='brands'>
-            <h3 className='title'>Navegue por marcas</h3>
-            <div className='images'>
-                <div><img src={brands} alt="" /></div>
-                <div><img src={brands} alt="" /></div>
-                <div><img src={brands} alt="" /></div>
-                <div><img src={brands} alt="" /></div>
-                <div><img src={brands} alt="" /></div>
-            </div>
+  return (
+    <div className="brands">
+      <h3 className="title">Navegue por marcas</h3>
+      <div className="images">
+        <div>
+          <img src={brands} alt="" />
         </div>
-    )
-}
+        <div>
+          <img src={brands} alt="" />
+        </div>
+        <div>
+          <img src={brands} alt="" />
+        </div>
+        <div>
+          <img src={brands} alt="" />
+        </div>
+        <div>
+          <img src={brands} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
